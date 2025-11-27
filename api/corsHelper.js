104a23a972b61f9c36852c4c81ac025fc1d3bd3f
2022-1-80-025 <2022-1-80-025@vlsi.loc>
2025-11-27 15:54:37 +0600

    Auto-commit: corsHelper.js updated

---
+++ api/corsHelper.js
@@ -0,0 +1,12 @@
+// CORS helper for Vercel serverless functions
+export function handleCors(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return true; // Indicate that the request was handled
+  }
+  return false; // Continue with normal processing
+}
