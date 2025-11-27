fd755da6d665c99632a17a6a5ca0db9023442a52
2022-1-80-025 <2022-1-80-025@vlsi.loc>
2025-11-27 16:15:51 +0600

    Auto-commit: corsHelper.js updated

--- api/corsHelper.js
+++ api/corsHelper.js
@@ -1,8 +1,9 @@
 // CORS helper for Vercel serverless functions
 export function handleCors(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://smt-doc-aid-amitubs-projects.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
